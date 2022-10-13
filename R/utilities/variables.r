19809e5621e32aca8a43db2ba218a0c1e3204707
Florentin Bulot <f.bulot@soton.ac.uk>
2022-10-13 13:03:13 +0100

    Demonstration of equivalence awithdifferent time averaging windows

--- R/utilities/variables.r
+++ R/utilities/variables.r
@@ -1,16 +1,15 @@
-FIDAS_PM_DATA_FILE <- "C:/Data/Fidas/promo/df_pm_2min.rds"
-FIDAS_SIZE_DATA_FILE <- "C:/Data/Fidas/promo/df_distrib_2min.rds"
-FIDAS_WEATHER_DATA_FILE <- "C:/Data/Fidas/promo/df_weather_2min.rds"
+FIDAS_PM_DATA_FILE <- "data/df_pm_2min.rds"
+FIDAS_SIZE_DATA_FILE <- "data/df_distrib_2min.rds"
+FIDAS_WEATHER_DATA_FILE <- "data/df_weather_2min.rds"
 
-SENSORS_DATA_FILE_10s <- "C:/Data/nocs/202007_to_202107_nocs_raw.rds"
-SENSORS_DATA_FILE <- "C:/Data/nocs/202007_to_202107_nocs.rds"
+SENSORS_DATA_FILE <- "data/202007_to_202107_nocs.rds"
 
 
-DF_JOINED <- "C:/Github/AQ_analysis_calibration/data/df_join.rds"
-DF_JOINED_ROLL <- "C:/Github/AQ_analysis_calibration/data/df_join_14min_roll.rds"
+DF_JOINED <- "data/df_join.rds"
+DF_JOINED_ROLL <- "data/df_join_14min_roll.rds"
 
 
 rm_uncertainty <-  0.48/sqrt(2)
 limit_value = 25
 
-PLOT_OUTPUT_FOLDER <- paste0(here::here(),"/output/full_year/plots/")
+PLOT_OUTPUT_FOLDER <- paste0(here::here(),"/output/plots/")
